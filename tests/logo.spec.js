db001d7409938b3b5d0c8002c59f2ee4b4b5dc11
Sergey Berezhnoy <veged@yandex-team.ru>
2022-04-09 00:53:52 +0300

    Initial commit

---
+++ tests/logo.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+const { test } = require('@playwright/test');
+
+
+test('make logo', async ({ page }) => {
+  await page.setViewportSize({ width: 2048, height: 1152 });
+  await page.goto('file://' + process.cwd() + '/logo.html');
+  await page.locator('.logo').screenshot({ path: 'logo.png' });
+});
