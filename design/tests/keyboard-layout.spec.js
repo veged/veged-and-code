57786f482220cdb882f15557a1eed374ebfc748b
Sergey Berezhnoy <veged@yandex-team.ru>
2024-01-23 04:01:47 +0300

    update keyboard layout icons

--- design/tests/keyboard-layout.spec.js
+++ design/tests/keyboard-layout.spec.js
@@ -1,20 +1,32 @@
 // @ts-check
 const { test } = require('@playwright/test')
+const { execSync } = require('node:child_process')
 
 
-test('make logo', async ({ page }) => {
+test('make keyboard layout icons', async ({ page }) => {
   await page.setViewportSize({ width: 2048, height: 1152 })
   await page.goto('file://' + process.cwd() + '/keyboard-layout/keyboard-layout.html')
 
   await page.screenshot({ path: 'icons.png' })
 
-  await page.locator('.icon_lang_en.icon_size_8').screenshot({ path: 'English.iconset/icon_16x16.png', omitBackground: true })
-  await page.locator('.icon_lang_en.icon_size_16').screenshot({ path: 'English.iconset/icon_16x16@2x.png', omitBackground: true })
-  await page.locator('.icon_lang_en.icon_size_16').screenshot({ path: 'English.iconset/icon_32x32.png', omitBackground: true })
-  await page.locator('.icon_lang_en.icon_size_32').screenshot({ path: 'English.iconset/icon_32x32@2x.png', omitBackground: true })
+  async function createIcon(lang, sourceSize, resultSize) {
+    const path = `keyboard-layout/${lang}.iconset/icon_${resultSize}.png`
+    await page.locator(`.icon_lang_${lang}.icon_size_${sourceSize}`).screenshot({ path, omitBackground: true })
+    execSync(`convert ${path} -alpha on -channel A -fx '1-(u.r+u.g+u.b)/3' ${path}`)
+    // execSync(`convert ${path} -negate ${path.replace('.png', '_dark.png')}`)
+  }
 
-  await page.locator('.icon_lang_ru.icon_size_8').screenshot({ path: 'Russian.iconset/icon_16x16.png', omitBackground: true })
-  await page.locator('.icon_lang_ru.icon_size_16').screenshot({ path: 'Russian.iconset/icon_16x16@2x.png', omitBackground: true })
-  await page.locator('.icon_lang_ru.icon_size_16').screenshot({ path: 'Russian.iconset/icon_32x32.png', omitBackground: true })
-  await page.locator('.icon_lang_ru.icon_size_32').screenshot({ path: 'Russian.iconset/icon_32x32@2x.png', omitBackground: true })
+  async function createIcons(lang, size) {
+    await createIcon(lang, size / 2, `${size}x${size}`)
+    await createIcon(lang, size, `${size}x${size}@2x`)
+  }
+
+  async function createIcns(lang) {
+    await createIcons(lang, 16)
+    await createIcons(lang, 32)
+    execSync(`iconutil -c icns keyboard-layout/${lang}.iconset`)
+  }
+
+  await createIcns('en')
+  await createIcns('ru')
 })
